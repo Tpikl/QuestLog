7e1da8eccda225130f1bb6bea21977afce7688c0
Tayler Nichols <taynich0@gmail.com>
2020-11-02 19:54:29 -0700

    Simple ui layout. Added sass.

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import { Navbar } from './shared/Navbar';
+import { Home } from './pages/Home';
+import { Weekly } from './pages/Weekly';
+
 
 export const App = (props) => {
   return (
 
     <Router>
 
-      <h1>React client</h1>
+      <Navbar />
+
+      <Route exact path='/' component={Home} />
+      <Route path='/weekly' component={Weekly} />
 
     </Router>
 
---
+++ Client/src/shared/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Navbar.scss';
+
+export const Navbar = () => {
+    return (
+        <nav>
+            <a href='/'>Home</a>
+            <a href='/weekly'>Weekly</a>
+        </nav>
+    );
+}
