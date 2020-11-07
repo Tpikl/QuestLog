e62cb1acfebd15d9a3682c847ea1931342a5f924
Tpikl <pickles.tayler@gmail.com>
2020-11-07 15:10:53 -0700

    Deconstructed Entry props. Fixed useEffect dependency.

--- Client/src/components/Entry.js
+++ Client/src/components/Entry.js
@@ -4,21 +4,21 @@ import { Reducer } from '../reducers/EntryFormReducer';
 import { Actions } from '../actions/EntryFormActions';
 import './Entry.scss';
 
-export const Entry = (props) => {
-    const [state, dispatch] = useReducer(Reducer, props.entry)
+export const Entry = ({entry, update, setFormEntry}) => {
+    const [state, dispatch] = useReducer(Reducer, entry)
     // Come back to this ---
     // useEffect(() => {
     //     dispatch({type: Actions.SET_ENTRY, value: props.entry});
     // }, [props.entry])
 
-    useEffect(() => updateEntry(), [state]);
+    useEffect(() => updateEntry(), [state.completed]);
     async function updateEntry() {
         UpdateEntry(state)
-            .then(() => props.update());
+            .then(() => update());
     }
     async function deleteEntry() {
         DeleteEntry(state.id)
-            .then(() => props.update());
+            .then(() => update());
     }
 
     return (
@@ -28,7 +28,7 @@ export const Entry = (props) => {
                     <b style={state.completed ? {textDecoration: 'line-through'}:{}}>{state.title}</b>
                 </span>
                 <div>
-                    <i className="fas fa-edit" onClick={() => props.setFormEntry(state)}></i>
+                    <i className="fas fa-edit" onClick={() => setFormEntry(state)}></i>
                     <i className="far fa-window-close" onClick={() => deleteEntry()}></i>
                 </div>
             </div>
