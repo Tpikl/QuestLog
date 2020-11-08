f912305e1ae129ee7fb77ec578ad5ad17ccc61a5
Tpikl <pickles.tayler@gmail.com>
2020-11-07 19:51:51 -0700

    Veritcal layout. Entry styled better.

--- Client/src/components/Entry.js
+++ Client/src/components/Entry.js
@@ -23,17 +23,19 @@ export const Entry = ({entry, update, setFormEntry}) => {
     }
 
     return (
-        <div className='entry'>
-            <div className='header'>
-                <span className={'title'} onClick={() => dispatch({type: Actions.FLIP_COMPLETED})}>
-                    <b style={state.completed ? {textDecoration: 'line-through'}:{}}>{state.title}</b>
-                </span>
-                <div>
-                    <i className="far fa-edit fa-lg" onClick={() => setFormEntry(state)}></i>
-                    <i className="far fa-window-close fa-lg" onClick={() => deleteEntry()}></i>
+        <div className={'entryBlock'}>
+            <div className='entry'>
+                <div className='header'>
+                    <span className={'title'} onClick={() => dispatch({type: Actions.FLIP_COMPLETED})}>
+                        <div style={state.completed ? {textDecoration: 'line-through'}:{}}>{state.title}</div>
+                    </span>
+                    <div className={'controls'}>
+                        <i className="far fa-edit fa-lg" onClick={() => setFormEntry(state)}></i>&nbsp;&nbsp;
+                        <i className="far fa-window-close fa-lg" onClick={() => deleteEntry()}></i>
+                    </div>
                 </div>
+                <small className={'description controls'} style={state.completed ? {textDecoration: 'line-through'}:{}}>{state.description}</small>
             </div>
-            <small className={'description'} style={state.completed ? {textDecoration: 'line-through'}:{}}>{state.description}</small>
         </div>
     )
 }
