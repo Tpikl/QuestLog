ffb5c6a1059e4c1a138ba5ad5a86de5e36b26bab
Tpikl <pickles.tayler@gmail.com>
2020-11-29 21:20:52 -0700

    Getting the monthly spread filled in. Not great, but will refactor.

--- QuestLog.Client/src/pages/Monthly.js
+++ QuestLog.Client/src/pages/Monthly.js
@@ -1,12 +1,37 @@
-import React, { useState } from 'react';
-import { addMonths, format, startOfMonth } from 'date-fns';
+import React, { useState, useEffect } from 'react';
+import { addMonths, endOfMonth, format, startOfMonth } from 'date-fns';
 
 import SpreadNav from '../components/SpreadNav';
 import StyledMonthly from './Monthly.styled';
-import { monthDays } from '../util/weekDays';
+import { monthDays, weeklyFormat } from '../util/weekDays';
+import EntryList from '../components/EntryList';
+import { DisplayAreas } from '../state/entry';
+import { ByDateRange } from '../api/entry';
+import { StyledEntryList } from '../components/EntryList.styled';
 
 const Monthly = () => {
     const [monthlyDate, setMonthlyDate] = useState(startOfMonth(new Date()));
+    useEffect(() => monthlyInit(), [monthlyDate]);
+
+    // Initialize Monthly data.
+    const [monthEntries, setMonthEntries] = useState([]);
+    function monthlyInit() {
+        ByDateRange(monthlyDate, endOfMonth(monthlyDate))
+        .then(r => {
+            setMonthEntries(r.data.map(e =>
+                ({...e, date: new Date(e.date)})
+            ));
+        });
+    }
+
+    const entriesByDay = (day) => {
+        let e = [];
+        monthEntries.forEach(item => {
+            if (item.date.getDate() === day && item.displayArea === DisplayAreas.Day.id)
+                e.push(item);
+        });
+        return e;
+    };
 
     return (
         <StyledMonthly>
@@ -21,8 +46,14 @@ const Monthly = () => {
             <div className='MonthList'>
                 {monthDays(monthlyDate).map((item, i) => {
                     return (
-                        <div key={i}>{format(item, 'do -')}</div>
-                    )
+                        <StyledEntryList key={i} boldTitle={false}>
+                        <EntryList
+                            area={{...DisplayAreas.day, name: weeklyFormat(item)}}
+                            day={item}
+                            entries={entriesByDay(i+1)}
+                            onSelect={() => {}}
+                            onUpdate={() => {}}/>
+                    </StyledEntryList>)
                 })}
             </div>
         </StyledMonthly>
