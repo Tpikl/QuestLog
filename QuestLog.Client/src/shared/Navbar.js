438510277f361b6c237a9c4c7e6d145355bcbe9f
Tpikl <pickles.tayler@gmail.com>
2020-12-13 22:48:29 -0700

    Resolved Monthly to Weekly date issue.

--- QuestLog.Client/src/shared/Navbar.js
+++ QuestLog.Client/src/shared/Navbar.js
@@ -5,9 +5,15 @@ import { NavLink } from 'react-router-dom';
 import './Navbar.scss';
 
 export const Navbar = ({date, setDate}) => {
+    const weeklyDate = () => {
+        return (new Date().getMonth() === date.getMonth())
+            ? new Date()
+            : date;
+    };
+
     return (<nav>
         <NavLink className='fancyBtn' activeClassName='activeLink' exact to='/'>Home</NavLink>
         <NavLink onClick={() => setDate(startOfMonth(date))} className='fancyBtn' activeClassName='activeLink' exact to='/Monthly'>Monthly</NavLink>
-        <NavLink onClick={() => setDate(startOfWeek(date))} className='fancyBtn' activeClassName='activeLink' exact to='/Weekly'>Weekly</NavLink>
+        <NavLink onClick={() => setDate(weeklyDate(date))} className='fancyBtn' activeClassName='activeLink' exact to='/Weekly'>Weekly</NavLink>
     </nav>);
 };
